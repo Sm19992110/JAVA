4a5119f200ab8b4b2f4c171176ad9f296041479b
Sm19992110 <105425763+Sm19992110@users.noreply.github.com>
2022-05-18 16:08:03 +0530

    Add files via upload

---
+++ Practical/ArithmaticOperator.java
@@ -0,0 +1,17 @@
+package Shubham3;
+
+public class ArithmaticOperator {
+
+public static void main(String[] args)
+{
+	int a=20;
+	int b=10;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	System.out.println(a%b);
+	
+}
+
+}
---
+++ Practical/DefandInitialization.java
@@ -0,0 +1,16 @@
+package Shubham3;
+
+public class DefandInitialization {
+	
+	public static void main(String[] args)
+	{
+			
+	int num1 = 12, num2 = 4;
+	System.out.println("num1 == num2 = " + (num1 == num2) );
+	System.out.println("num1 != num2 = " + (num1 != num2) );
+	System.out.println("num1 >  num2 = " + (num1 >  num2) );
+	System.out.println("num1 <  num2 = " + (num1 <  num2) );
+    System.out.println("num1 >= num2 = " + (num1 >= num2) );
+	System.out.println("num1 <= num2 = " + (num1 <= num2) );
+	}
+}
