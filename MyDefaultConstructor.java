e1f07e3a3f05b98fb78f6b4c679e96bb068d9537
Sm19992110 <105425763+Sm19992110@users.noreply.github.com>
2022-05-19 17:19:19 +0530

    Add files via upload

---
+++ MyDefaultConstructor.java
@@ -0,0 +1,10 @@
+package Shubham3;
+
+public class MyDefaultConstructor {
+          public MyDefaultConstructor() {
+        	  System.out.println("this is Default Constructor");
+          }
+  public static void main(String a[]) {
+	  MyDefaultConstructor mdc = new MyDefaultConstructor();
+  }
+}
